d083a90d6eec2276ecd7ff7a0e296d23af76524c
unknown <thduszbzb12@gmail.com>
2026-01-02 16:08:10 +0900

    Fix:merge

--- src/main/java/com/example/spider_spring/repository/HeartbeatRepository.java
+++ src/main/java/com/example/spider_spring/repository/HeartbeatRepository.java
@@ -1,12 +1,14 @@
 package com.example.spider_spring.repository;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.spider_spring.domain.Heartbeat;
+
 @Repository
-public interface HeartbeatRepository {
-	void updateLastSeen(Integer machineId, LocalDateTime now);
-	
-	LocalDateTime findLastSeen(Integer machineId);
+public interface HeartbeatRepository extends JpaRepository<Heartbeat, Integer> {
+
+	Optional<Heartbeat> findByMachineId(Integer machineId);
 }
